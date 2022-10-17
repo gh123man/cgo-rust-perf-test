5aa380790ce944dab831717612d8314c0d7f9862
Brian Floersch <brian.floersch@datadoghq.com>
2022-10-17 15:41:29 -0400

    remove dead code

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,8 @@ lazy_static! {
 #[no_mangle]
 pub extern "C" fn transform(input: *const libc::c_char) -> *const libc::c_char {
     let inpt: &CStr = unsafe { CStr::from_ptr(input) };
-
-    // let mut rust_str = inpt.to_str().unwrap().to_string();
-
-    // let re = Regex::new(r"\b\w{4}\b").unwrap();
     let replaced = RE.replace(inpt.to_str().unwrap(), "rust");
-
     let c_str = CString::new(replaced.as_bytes()).expect("CString::new failed");
-    // let c_str = CString::new(rust_str).expect("CString::new failed");
     return c_str.into_raw();
 }
 
