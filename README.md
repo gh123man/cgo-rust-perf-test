5d0cabe663085ba89f59ad571939c3f0acbb65a3
Brian Floersch <brian.floersch@datadoghq.com>
2022-10-17 15:48:48 -0400

    expand benchmarks

--- README.md
+++ README.md
@@ -18,10 +18,30 @@ To run the test scripts
 goos: darwin
 goarch: arm64
 pkg: cgotest
-BenchmarkRustRegex-10          	       9	 120307569 ns/op	11840053 B/op	   30000 allocs/op
-BenchmarkGoRegex-10            	       5	 202554858 ns/op	43821992 B/op	   70044 allocs/op
-BenchmarkRustPassthrough-10    	     164	   7245905 ns/op	11840022 B/op	   30000 allocs/op
-BenchmarkGoPassthrough-10      	    1059	   1154868 ns/op	11520027 B/op	   10000 allocs/op
+BenchmarkRustRegex1-10               	   83535	     12462 ns/op	    1184 B/op	       3 allocs/op
+BenchmarkRustRegex10-10              	    9603	    124965 ns/op	   11840 B/op	      30 allocs/op
+BenchmarkRustRegex100-10             	     970	   1236614 ns/op	  118402 B/op	     300 allocs/op
+BenchmarkRustRegex1000-10            	      90	  12440297 ns/op	 1184008 B/op	    3000 allocs/op
+BenchmarkRustRegex10000-10           	       9	 120249370 ns/op	11840042 B/op	   30000 allocs/op
+BenchmarkRustRegex100000-10          	       1	1182302042 ns/op	118400576 B/op	  300006 allocs/op
+BenchmarkGoRegex1-10                 	   57272	     20347 ns/op	    4381 B/op	       7 allocs/op
+BenchmarkGoRegex10-10                	    5848	    204508 ns/op	   43803 B/op	      70 allocs/op
+BenchmarkGoRegex100-10               	     594	   2053824 ns/op	  438393 B/op	     700 allocs/op
+BenchmarkGoRegex1000-10              	      58	  20642690 ns/op	 4383709 B/op	    7004 allocs/op
+BenchmarkGoRegex10000-10             	       5	 204252575 ns/op	43866638 B/op	   70051 allocs/op
+BenchmarkGoRegex100000-10            	       1	2033331541 ns/op	438069768 B/op	  700412 allocs/op
+BenchmarkRustPassthrough1-10         	 1638686	       728.7 ns/op	    1184 B/op	       3 allocs/op
+BenchmarkRustPassthrough10-10        	  165696	      7460 ns/op	   11840 B/op	      30 allocs/op
+BenchmarkRustPassthrough100-10       	   16524	     73184 ns/op	  118400 B/op	     300 allocs/op
+BenchmarkRustPassthrough1000-10      	    1580	    729162 ns/op	 1184001 B/op	    3000 allocs/op
+BenchmarkRustPassthrough10000-10     	     164	   7358990 ns/op	11840024 B/op	   30000 allocs/op
+BenchmarkRustPassthrough100000-10    	      15	  73081711 ns/op	118400243 B/op	  300002 allocs/op
+BenchmarkGoPassthrough1-10           	 9461560	       121.0 ns/op	    1152 B/op	       1 allocs/op
+BenchmarkGoPassthrough10-10          	 1000000	      1175 ns/op	   11520 B/op	      10 allocs/op
+BenchmarkGoPassthrough100-10         	  103849	     12657 ns/op	  115200 B/op	     100 allocs/op
+BenchmarkGoPassthrough1000-10        	    9368	    125586 ns/op	 1152003 B/op	    1000 allocs/op
+BenchmarkGoPassthrough10000-10       	    1030	   1225275 ns/op	11520027 B/op	   10000 allocs/op
+BenchmarkGoPassthrough100000-10      	     100	  12555695 ns/op	115200277 B/op	  100002 allocs/op
 PASS
-ok  	cgotest	7.443s
+ok  	cgotest	33.895s
 ```
--- main_test.go
+++ main_test.go
@@ -4,34 +4,61 @@ import "testing"
 
 // Run `cargo build` first!
 
-func BenchmarkRustRegex(b *testing.B) {
+func benchmarkRustRegex(j int, b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < 10000; i++ {
+		for i := 0; i < j; i++ {
 			processStringRs("Oct 17 14:33:33 | XSS | ERROR | (/viral/interactive/deliverables/holistic.go:3) | sed et dolorem minima et corrupti abcd veniam qui blanditiis optio explicabo et amet qui sint ut iure neque eveniet quod odio distinctio quas veniam voluptatibus quibusdam esse maiores dolores magni numquam sed deserunt quia odio fuga deserunt cumque a aliquam ad dolores dolore aut sapiente necessitatibus ut autem necessitatibus quam eveniet et omnis aut quos dolorem culpa nostrum quas provident tempora voluptate iure quos iste consequatur minima accusantium molestiae consequatur perspiciatis quis quia at incidunt non veritatis deserunt totam iure autem asperiores rerum officiis iusto et explicabo sunt et rerum molestiae hic dolore neque eum vel rerum perspiciatis autem et consequuntur consequatur aliquam dolore magni ea est illum accusamus rerum magnam neque odio voluptatibus est temporibus quo ullam nobis soluta quo ipsum temporibus perferendis et esse repellendus ea id explicabo nostrum repellat vero perferendis possimus optio consectetur deserunt aspern")
 		}
 	}
 }
 
-func BenchmarkGoRegex(b *testing.B) {
+func BenchmarkRustRegex1(b *testing.B)      { benchmarkRustRegex(1, b) }
+func BenchmarkRustRegex10(b *testing.B)     { benchmarkRustRegex(10, b) }
+func BenchmarkRustRegex100(b *testing.B)    { benchmarkRustRegex(100, b) }
+func BenchmarkRustRegex1000(b *testing.B)   { benchmarkRustRegex(1000, b) }
+func BenchmarkRustRegex10000(b *testing.B)  { benchmarkRustRegex(10000, b) }
+func BenchmarkRustRegex100000(b *testing.B) { benchmarkRustRegex(100000, b) }
+
+func benchmarkGoRegex(j int, b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < 10000; i++ {
+		for i := 0; i < j; i++ {
 			processStringGo("Oct 17 14:33:33 | XSS | ERROR | (/viral/interactive/deliverables/holistic.go:3) | sed et dolorem minima et corrupti abcd veniam qui blanditiis optio explicabo et amet qui sint ut iure neque eveniet quod odio distinctio quas veniam voluptatibus quibusdam esse maiores dolores magni numquam sed deserunt quia odio fuga deserunt cumque a aliquam ad dolores dolore aut sapiente necessitatibus ut autem necessitatibus quam eveniet et omnis aut quos dolorem culpa nostrum quas provident tempora voluptate iure quos iste consequatur minima accusantium molestiae consequatur perspiciatis quis quia at incidunt non veritatis deserunt totam iure autem asperiores rerum officiis iusto et explicabo sunt et rerum molestiae hic dolore neque eum vel rerum perspiciatis autem et consequuntur consequatur aliquam dolore magni ea est illum accusamus rerum magnam neque odio voluptatibus est temporibus quo ullam nobis soluta quo ipsum temporibus perferendis et esse repellendus ea id explicabo nostrum repellat vero perferendis possimus optio consectetur deserunt aspern")
 		}
 	}
 }
 
-func BenchmarkRustPassthrough(b *testing.B) {
+func BenchmarkGoRegex1(b *testing.B)      { benchmarkGoRegex(1, b) }
+func BenchmarkGoRegex10(b *testing.B)     { benchmarkGoRegex(10, b) }
+func BenchmarkGoRegex100(b *testing.B)    { benchmarkGoRegex(100, b) }
+func BenchmarkGoRegex1000(b *testing.B)   { benchmarkGoRegex(1000, b) }
+func BenchmarkGoRegex10000(b *testing.B)  { benchmarkGoRegex(10000, b) }
+func BenchmarkGoRegex100000(b *testing.B) { benchmarkGoRegex(100000, b) }
+
+func benchmarkRustPassthrough(j int, b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < 10000; i++ {
+		for i := 0; i < j; i++ {
 			simpleStringRs("Oct 17 14:33:33 | XSS | ERROR | (/viral/interactive/deliverables/holistic.go:3) | sed et dolorem minima et corrupti abcd veniam qui blanditiis optio explicabo et amet qui sint ut iure neque eveniet quod odio distinctio quas veniam voluptatibus quibusdam esse maiores dolores magni numquam sed deserunt quia odio fuga deserunt cumque a aliquam ad dolores dolore aut sapiente necessitatibus ut autem necessitatibus quam eveniet et omnis aut quos dolorem culpa nostrum quas provident tempora voluptate iure quos iste consequatur minima accusantium molestiae consequatur perspiciatis quis quia at incidunt non veritatis deserunt totam iure autem asperiores rerum officiis iusto et explicabo sunt et rerum molestiae hic dolore neque eum vel rerum perspiciatis autem et consequuntur consequatur aliquam dolore magni ea est illum accusamus rerum magnam neque odio voluptatibus est temporibus quo ullam nobis soluta quo ipsum temporibus perferendis et esse repellendus ea id explicabo nostrum repellat vero perferendis possimus optio consectetur deserunt aspern")
 		}
 	}
 }
 
-func BenchmarkGoPassthrough(b *testing.B) {
+func BenchmarkRustPassthrough1(b *testing.B)      { benchmarkRustPassthrough(1, b) }
+func BenchmarkRustPassthrough10(b *testing.B)     { benchmarkRustPassthrough(10, b) }
+func BenchmarkRustPassthrough100(b *testing.B)    { benchmarkRustPassthrough(100, b) }
+func BenchmarkRustPassthrough1000(b *testing.B)   { benchmarkRustPassthrough(1000, b) }
+func BenchmarkRustPassthrough10000(b *testing.B)  { benchmarkRustPassthrough(10000, b) }
+func BenchmarkRustPassthrough100000(b *testing.B) { benchmarkRustPassthrough(100000, b) }
+
+func benchmarkGoPassthrough(j int, b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < 10000; i++ {
+		for i := 0; i < j; i++ {
 			simpleStringGo("Oct 17 14:33:33 | XSS | ERROR | (/viral/interactive/deliverables/holistic.go:3) | sed et dolorem minima et corrupti abcd veniam qui blanditiis optio explicabo et amet qui sint ut iure neque eveniet quod odio distinctio quas veniam voluptatibus quibusdam esse maiores dolores magni numquam sed deserunt quia odio fuga deserunt cumque a aliquam ad dolores dolore aut sapiente necessitatibus ut autem necessitatibus quam eveniet et omnis aut quos dolorem culpa nostrum quas provident tempora voluptate iure quos iste consequatur minima accusantium molestiae consequatur perspiciatis quis quia at incidunt non veritatis deserunt totam iure autem asperiores rerum officiis iusto et explicabo sunt et rerum molestiae hic dolore neque eum vel rerum perspiciatis autem et consequuntur consequatur aliquam dolore magni ea est illum accusamus rerum magnam neque odio voluptatibus est temporibus quo ullam nobis soluta quo ipsum temporibus perferendis et esse repellendus ea id explicabo nostrum repellat vero perferendis possimus optio consectetur deserunt aspern")
 		}
 	}
 }
+func BenchmarkGoPassthrough1(b *testing.B)      { benchmarkGoPassthrough(1, b) }
+func BenchmarkGoPassthrough10(b *testing.B)     { benchmarkGoPassthrough(10, b) }
+func BenchmarkGoPassthrough100(b *testing.B)    { benchmarkGoPassthrough(100, b) }
+func BenchmarkGoPassthrough1000(b *testing.B)   { benchmarkGoPassthrough(1000, b) }
+func BenchmarkGoPassthrough10000(b *testing.B)  { benchmarkGoPassthrough(10000, b) }
+func BenchmarkGoPassthrough100000(b *testing.B) { benchmarkGoPassthrough(100000, b) }
