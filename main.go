0e96616ca24904540cc0351a3ee6ae129ae2d611
Scott Opell <me@scottopell.com>
2022-10-27 15:49:39 -0400

    Fix rpath argument to be cross-compiler compatible

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 //#cgo CFLAGS: -I${SRCDIR}/target/release/
-//#cgo LDFLAGS: -L${SRCDIR}/target/release -Wl,-rpath=${SRCDIR}/target/release -lhelloRust
+//#cgo LDFLAGS: -L${SRCDIR}/target/release -Wl,-rpath,${SRCDIR}/target/release -lhelloRust
 //
 //#include <stdio.h>
 //#include <stdlib.h>
