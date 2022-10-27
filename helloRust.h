29a4bc7b57e04ec270ef9fa8a5db82dcffb553b0
Scott Opell <me@scottopell.com>
2022-10-27 16:22:39 +0000

    Adds UDS and a no-stdout mode with builtin throughput

--- helloRust.h
+++ helloRust.h
@@ -1,4 +1,5 @@
 
 char* transform(char* str);
 char* passthrough(char* str);
+char* noop(char* str);
 char* transform_vrl(char* str);
--- main.go
+++ main.go
@@ -12,42 +12,152 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"log"
+	"net"
 	"os"
 	"regexp"
+	"runtime"
 	"strings"
+	"time"
 	"unsafe"
 
 	"github.com/benthosdev/benthos/v4/public/bloblang"
+	"github.com/dustin/go-humanize"
+	"go.uber.org/atomic"
 )
+
+const (
+	sockAddr = "/tmp/cgo.sock"
+)
+
+func getUdsReader() *bufio.Reader {
+	if _, err := os.Stat(sockAddr); err == nil {
+		if err := os.RemoveAll(sockAddr); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	listener, err := net.Listen("unix", sockAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Waiting for connection...")
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Accepted connection from ", conn.RemoteAddr().Network())
+
+	return bufio.NewReader(conn)
+}
+
+type throughputRecorder struct {
+	start      time.Time
+	totalBytes atomic.Float64
+}
+
+func (tr *throughputRecorder) Record(nBytes int) {
+	if time.Time.IsZero(tr.start) {
+		tr.start = time.Now()
+	}
+	tr.totalBytes.Add(float64(nBytes))
+}
+
+func (tr *throughputRecorder) AvgThroughput() string {
+	now := time.Now()
+
+	elapsed := now.Sub(tr.start)
+	avgBytes := uint64(tr.totalBytes.Load() / elapsed.Seconds())
+	return fmt.Sprintf("%s / second", humanize.Bytes(avgBytes))
+}
+
+func getBlackholeWriter(tr *throughputRecorder) func(a ...any) (int, error) {
+
+	blackhole := func(a ...any) (int, error) {
+		s := a[0].(string)
+		l := len(s)
+		tr.Record(l)
+		return l, nil
+	}
+
+	return blackhole
+}
+
+type OutFunc func(a ...any) (int, error)
 
 func main() {
 
 	rust := flag.Bool("rust", false, "use rust")
+	noopRust := flag.Bool("nooprust", false, "use no-op rust")
+	noopGo := flag.Bool("noopgo", false, "use no-op go")
 	vrl := flag.Bool("vrl", false, "use vrl")
+	stdout := flag.Bool("stdout", false, "Output to stdout")
 	useBloblang := flag.Bool("bloblang", false, "use bloblang")
+	useUds := flag.Bool("uds", false, "accept data from UDS")
 	flag.Parse()
 
-	reader := bufio.NewReader(os.Stdin)
+	var reader *bufio.Reader
+	if *useUds {
+		reader = getUdsReader()
+	} else {
+		reader = bufio.NewReader(os.Stdin)
+	}
 
 	var exe *bloblang.Executor
 	if *useBloblang {
 		exe = setupBloblang()
 	}
 
+	var output OutFunc
+	if *stdout {
+		output = fmt.Println
+	} else {
+		throughputRecorder := throughputRecorder{}
+		output = getBlackholeWriter(&throughputRecorder)
+		go func() {
+			oneSecond, err := time.ParseDuration("1s")
+			if err != nil {
+				panic(err)
+			}
+
+			for {
+				time.Sleep(oneSecond)
+				fmt.Println(throughputRecorder.AvgThroughput())
+			}
+		}()
+	}
+
 	for {
 		text, _ := reader.ReadString('\n')
 		if *rust {
-			fmt.Println(processStringRs(text))
+			output(processStringRs(text))
 		} else if *vrl {
 			text = strings.TrimSpace(text)
 			text = fmt.Sprintf("{\"message\":\"%s\"}", text)
-			fmt.Println(processStringVrl(text))
+			output(processStringVrl(text))
 		} else if *useBloblang {
-			fmt.Println(processStringBloblang(exe, text))
+			output(processStringBloblang(exe, text))
+		} else if *noopRust {
+			output(noopStringRs(text))
+		} else if *noopGo {
+			output(simpleStringGo(text))
 		} else {
-			fmt.Println(processStringGo(text))
+			output(processStringGo(text))
 		}
+
+		runtime.Gosched()
 	}
+}
+
+func noopStringRs(str string) string {
+	cs := C.CString(str)
+	b := C.noop(cs)
+	s := C.GoString(b)
+	defer C.free(unsafe.Pointer(cs))
+	defer C.free(unsafe.Pointer(b))
+	return s
 }
 
 func processStringRs(str string) string {
